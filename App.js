2107cf08d19eaf4d23110fff4f4126ea5382c88d
Caheri <caheri.aguilar@gmail.com>
2024-08-07 14:44:17 -0500

    created cover page

--- App.js
+++ App.js
@@ -1,20 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { Button, PaperProvider } from "react-native-paper";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider>
+      <View style={styles.container}>
+        <Text style={styles.heading}>Organize Your</Text>
+        <Text style={styles.heading}>Tasks with SQLite</Text>
+        <Text>Designed for Happiness, Not Just Profuctivity.</Text>
+        <Text>Enjoy a Stress-free Way to Manage Your Day.</Text>
+        <Button
+          style={styles.button}
+          buttonColor="#6BA2EA"
+          textColor="white"
+          onPress={() => console.log("Get started button pressed")}
+        >
+          Get started
+        </Button>
+        <StatusBar style="auto" />
+      </View>
+    </PaperProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "flex-start",
+    justifyContent: "center",
+    paddingLeft: 15,
+  },
+  heading: {
+    fontWeight: "bold",
+    fontSize: 40,
+    marginBottom: 5,
+  },
+  button: {
+    position: "absolute",
+    bottom: 70, // 50 units from the top
+    right: 20, // 20 units from the left
   },
 });
